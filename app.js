deb67d68b4ce1a906a1c4605537e404d2222a6cc
Jere1 <monserratjere@gmail.com>
2023-10-27 13:23:22 -0300

    Portfolio propio

---
+++ app.js
@@ -0,0 +1,213 @@
+//funcion que carga las animaciones de las habilidades
+function cargarAnimaciones(){
+    $(function(){
+        $('.chart1').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart2').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart3').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart4').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart5').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart6').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+
+    })
+}
+//Función para filtrar las categorías de los trabajos
+function verCategoria(cat){
+    const items = document.getElementsByClassName("item");
+    for(let i=0; i < items.length;i++){
+        items[i].style.display = "none";
+    }
+
+    const itemCat = document.getElementsByClassName(cat);
+    for(let i = 0; i<itemCat.length;i++){
+        itemCat[i].style.display = "block";
+    }
+
+    const links = document.querySelectorAll(".trabajos nav a");
+    links[0].className = "";
+    links[1].className = "";
+    links[2].className = "";
+    links[3].className = "";
+
+    const itemSeleccionado = document.getElementById(cat);
+    itemSeleccionado.className = "borde";
+}
+
+//Función que detecta cuando lanzar la animación de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("chart");
+        habilidades[0].classList.add("chart1");
+        habilidades[1].classList.add("chart2");
+        habilidades[2].classList.add("chart3");
+        habilidades[3].classList.add("chart4");
+        habilidades[4].classList.add("chart5");
+        habilidades[5].classList.add("chart6");
+
+        cargarAnimaciones();
+    }
+}
+
+//detecto el scrolling para aplicar la animación de las habilidaes
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//función que muestra el menu responsive{
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+
+
+        //creamos el elemento que cierra el menu
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        document.getElementById("nav").appendChild(span);
+
+        //quitamos el boton eliminar cuando se hace click sobre este
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+}
+
+//Este codigo es para agregar una función a cada links del menu
+//responsive
+//cuando se presione en cualquier de los links del menu debe desaparecer el menu
+const links = document.querySelectorAll("#nav a");
+for(i=0; i < links.length;i++){
+    links[i].onclick = function(){
+        var x = document.getElementById("nav");
+        x.className = "";
+
+        //removemos el boton eliminar
+        btnEliminar = document.querySelector("#nav span");
+        btnEliminar.remove();
+    }
+}
+
+//BASE DATOS
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBse4TKa6FSqb7HZu80tiE8tI84akh0YCU",
+    authDomain: "portfolio-63255.firebaseapp.com",
+    projectId: "portfolio-63255",
+    storageBucket: "portfolio-63255.appspot.com",
+    messagingSenderId: "569671307844",
+    appId: "1:569671307844:web:fdf1c8cae94a65d7406211",
+    measurementId: "G-72LKG8E58T"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
+document.getElementById('formulario').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Validar campo nombre
+    let entradaNombre = document.getElementById('name');
+    let errorNombre = document.getElementById('nameError');
+
+    if (entradaNombre.value.trim() === '') {
+        errorNombre.textContent = 'Por favor, introduce tu nombre';
+        errorNombre.classList.add('error-message');
+    } else {
+        errorNombre.textContent = '';
+        errorNombre.classList.remove('error-message');
+    }
+
+    // Validar correo electrónico
+    let emailEntrada = document.getElementById('email');
+    let emailError = document.getElementById('emailError');
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(emailEntrada.value)) {
+        emailError.textContent = 'Por favor, introduce un email válido';
+        emailError.classList.add('error-message');
+    } else {
+        emailError.textContent = '';
+        emailError.classList.remove('error-message');
+    }
+
+
+    let mensajeEntrada = document.getElementById('mensaje');
+
+    if (mensajeEntrada.value.length) {
+
+    // Si todos los campos son válidos, enviar formulario
+    if (!errorNombre.textContent && !emailError.textContent && !mensajeEntrada.textContent) {
+        // BACK END QUE RECIBA LA INFO
+
+        db.collection("users").add({
+            nombre: entradaNombre.value,
+            email: emailEntrada.value,
+            mensaje: mensajeEntrada.value 
+        })
+        .then((docRef) => {
+            alert('¡Muchas gracias por comunicarte!', docRef.id);
+            document.getElementById('formulario').reset();
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+    }
+}
+});
+
